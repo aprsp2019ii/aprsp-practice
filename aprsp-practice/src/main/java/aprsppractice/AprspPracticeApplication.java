a60a595b6f25ca19d22aa8c8d3efe04322c7f431
Miroslav Stefanovic <mstef@uns.ac.rs>
2019-03-03 14:40:52 +0100

    Initial exercise project setup for APRSP

---
+++ aprsp-practice/src/main/java/aprsppractice/AprspPracticeApplication.java
@@ -0,0 +1,13 @@
+package aprsppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprspPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprspPracticeApplication.class, args);
+	}
+
+}
---
+++ aprsp-practice/src/test/java/aprsppractice/AprspPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package aprsppractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AprspPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
